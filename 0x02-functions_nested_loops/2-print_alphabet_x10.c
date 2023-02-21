8cf89db15adc5c7c7725798e48b432728b07fb61
KorirMoze <korirkipsmoze@gmail.com>
2023-02-21 03:03:03 -0800

    2nd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*always-check the code
+*Return aways(0)
+*/
+int main(void)
+{
+	alpha_count = 0;
+	while (alpha_count <10;)
+	{
+		char al = 'a';
+		while(al <='z')
+		{
+			_putchar(al);
+			al++;
+		}
+		alpha_count++;
+		_putchar('\n')
+	}
+}
